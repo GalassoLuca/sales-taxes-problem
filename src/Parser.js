29f207704d632f6e936ba73e3524e29c71741ab6
Luca Galasso <galasso.luca@outlook.com>
2018-03-25 11:11:53 +0200

    parser’s structure

---
+++ src/Parser.js
@@ -0,0 +1,12 @@
+const Basket = require('./Basket')
+const Item = require('./Item')
+const CATEGORIES = require('../config/categories.json')
+
+module.exports = class Parser {
+	get parseBasket(text) {
+
+	}
+	get parseItem(lineText) {
+
+	}
+}
