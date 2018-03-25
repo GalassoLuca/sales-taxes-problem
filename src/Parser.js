afceec3d646838de15cb16aad5ae05bb7925eec5
Luca Galasso <galasso.luca@outlook.com>
2018-03-25 18:48:20 +0200

    basic parser for a single line that contain an item

--- src/Parser.js
+++ src/Parser.js
@@ -4,9 +4,42 @@ const CATEGORIES = require('../config/categories.json')
 
 module.exports = class Parser {
 	static parseBasket(text) {
-		
+
 	}
 	static parseItem(lineText) {
+		const textArray = lineText.split(' ')
+		let i = 0
 
+		const quantity = parseInt(textArray[i++])
+		let isImported = false
+		if (textArray[i] == 'imported'){
+			isImported = true
+			i++
+		}
+
+		const name = textArray.slice(i, textArray.length - 2).join(' ')
+		const hasSalesTax = Parser._hasSalesTax(name)
+		
+		const price = parseFloat(textArray[textArray.length - 1])
+		return new Item(name, quantity, price, hasSalesTax, isImported)
+	}
+
+	static _hasSalesTax(name){
+		let hasSalesTax = true
+
+		CATEGORIES.books.forEach(function(keyword) {
+			if (name == keyword)
+				hasSalesTax = false
+		})
+		CATEGORIES.food.forEach(function(keyword) {
+			if (name == keyword)
+				hasSalesTax = false
+		})
+		CATEGORIES.medical.forEach(function(keyword) {
+			if (name == keyword)
+				hasSalesTax = false
+		})
+
+		return hasSalesTax
 	}
 }
